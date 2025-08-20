072e7fa2817302303e91ad6c414ae1ea8e12e165
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2025-08-20 22:54:34 +0700

    initial init

---
+++ src/main/java/com/youapp/YouApp/YouAppApplication.java
@@ -0,0 +1,13 @@
+package com.youapp.YouApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youapp/YouApp/YouAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youapp.YouApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
